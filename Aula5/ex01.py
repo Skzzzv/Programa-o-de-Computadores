868bb2428f9649fa270ef24d21176b4285bf0636
Skzzzv <eduardoems203@gmail.com>
2025-10-01 22:00:55 -0300

    a

---
+++ Aula5/ex01.py
@@ -0,0 +1,21 @@
+x = int(input('Digite o valor de x: '))
+y = int(input('Digite o valor de y: '))
+
+if (x>0) and y>0: 
+    print(f'Você está no primeiro quadrante.')
+
+elif x<0 and y>0:
+    print(f'Você está no segundo quadrante.')
+
+elif x<0 and y<0:
+    print('Você está no terceiro quadrante.')
+
+elif x>0 and y<0:
+    print(f'Você está no quarto quadrante.')
+
+elif x==0 and y==0: 
+    print('Você está na origem.')
+elif x==0:
+    print('Você está no eixo x.')
+else: 
+    print('Você está no eixo y.')
